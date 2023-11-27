a5541c67cd05c5d717db96db1c35049e3541ec3a
Shin Hung <88814120+aws1728@users.noreply.github.com>
2023-11-27 18:52:32 +0800

    Add files via upload

--- project/project.ino
+++ project/project.ino
@@ -13,7 +13,8 @@ int PosSave [Address_Max + 1][6];
 int LastPos [6];
 int WriteAddress = 0;
 int ReadAddress = 0;
-int stepSize = 1;
+int StepSize = 1;
+int StepTime = 0;
 int Position_A = 90;
 int Position_B = 0; 
 int Position_C = 180;
@@ -26,7 +27,7 @@ int MovingTime_C = 0;
 int MovingTime_D = 0;
 int MovingTime_E = 0;
 int MovingTime_F = 0;
-int WriteCheck = 0;
+int WaitCheck_A = 0;
 int WaitTime = 0;
 int Commed = 0;
 char receivedData[50];
@@ -67,57 +68,57 @@ void loop() {
     Serial.print("F:" + String(Position_F) + " ");
     Serial.println("Commed:" + String(Commed));
   
-    if(Commed == 0){
+    if (Commed == 0) {
       Serial.println("Commed == 0");
-      //int currentPosition_A = servo_A.read();  //讀取伺服馬達的當前位置
-      int currentPosition_A = LastPos[0];
-      moveToTargetPosition_A(currentPosition_A, Position_A, stepSize);
+      //int CurrentPosition_A = servo_A.read();  //讀取伺服馬達的當前位置
+      int CurrentPosition_A = LastPos[0];
+      moveToTargetPosition_A(CurrentPosition_A, Position_A, StepSize);
       
-      //int currentPosition_B = servo_B.read();  //讀取伺服馬達的當前位置
-      int currentPosition_B = LastPos[1];
-      moveToTargetPosition_B(currentPosition_B, Position_B, stepSize);
+      //int CurrentPosition_B = servo_B.read();  //讀取伺服馬達的當前位置
+      int CurrentPosition_B = LastPos[1];
+      moveToTargetPosition_B(CurrentPosition_B, Position_B, StepSize);
     
-      //int currentPosition_C = servo_C.read();  //讀取伺服馬達的當前位置
-      int currentPosition_C = LastPos[2];
-      moveToTargetPosition_C(currentPosition_C, Position_C, stepSize);
+      //int CurrentPosition_C = servo_C.read();  //讀取伺服馬達的當前位置
+      int CurrentPosition_C = LastPos[2];
+      moveToTargetPosition_C(CurrentPosition_C, Position_C, StepSize);
     
-      //int currentPosition_D = servo_D.read();  //讀取伺服馬達的當前位置
-      int currentPosition_D = LastPos[3];
-      moveToTargetPosition_D(currentPosition_D, Position_D, stepSize);
+      //int CurrentPosition_D = servo_D.read();  //讀取伺服馬達的當前位置
+      int CurrentPosition_D = LastPos[3];
+      moveToTargetPosition_D(CurrentPosition_D, Position_D, StepSize);
     
-      //int currentPosition_E = servo_E.read();  //讀取伺服馬達的當前位置
-      int currentPosition_E = LastPos[4];
-      moveToTargetPosition_E(currentPosition_E, Position_E, stepSize);
+      //int CurrentPosition_E = servo_E.read();  //讀取伺服馬達的當前位置
+      int CurrentPosition_E = LastPos[4];
+      moveToTargetPosition_E(CurrentPosition_E, Position_E, StepSize);
     
-      //int currentPosition_F = servo_F.read();  //讀取伺服馬達的當前位置
-      int currentPosition_F = LastPos[5];
-      moveToTargetPosition_F(currentPosition_F, Position_F, stepSize);
+      //int CurrentPosition_F = servo_F.read();  //讀取伺服馬達的當前位置
+      int CurrentPosition_F = LastPos[5];
+      moveToTargetPosition_F(CurrentPosition_F, Position_F, StepSize);
       }
     
-    else if (Commed == 1){
-      //int currentPosition_A = servo_A.read();  //讀取伺服馬達的當前位置
-      int currentPosition_A = LastPos[0];
-      moveToTargetPosition_A(currentPosition_A, Position_A, stepSize);
+    else if (Commed == 1) {
+      //int CurrentPosition_A = servo_A.read();  //讀取伺服馬達的當前位置
+      int CurrentPosition_A = LastPos[0];
+      moveToTargetPosition_A(CurrentPosition_A, Position_A, StepSize);
       
-      //int currentPosition_B = servo_B.read();  //讀取伺服馬達的當前位置
-      int currentPosition_B = LastPos[1];
-      moveToTargetPosition_B(currentPosition_B, Position_B, stepSize);
+      //int CurrentPosition_B = servo_B.read();  //讀取伺服馬達的當前位置
+      int CurrentPosition_B = LastPos[1];
+      moveToTargetPosition_B(CurrentPosition_B, Position_B, StepSize);
     
-      //int currentPosition_C = servo_C.read();  //讀取伺服馬達的當前位置
-      int currentPosition_C = LastPos[2];
-      moveToTargetPosition_C(currentPosition_C, Position_C, stepSize);
+      //int CurrentPosition_C = servo_C.read();  //讀取伺服馬達的當前位置
+      int CurrentPosition_C = LastPos[2];
+      moveToTargetPosition_C(CurrentPosition_C, Position_C, StepSize);
     
-      //int currentPosition_D = servo_D.read();  //讀取伺服馬達的當前位置
-      int currentPosition_D = LastPos[3];
-      moveToTargetPosition_D(currentPosition_D, Position_D, stepSize);
+      //int CurrentPosition_D = servo_D.read();  //讀取伺服馬達的當前位置
+      int CurrentPosition_D = LastPos[3];
+      moveToTargetPosition_D(CurrentPosition_D, Position_D, StepSize);
     
-      //int currentPosition_E = servo_E.read();  //讀取伺服馬達的當前位置
-      int currentPosition_E = LastPos[4];
-      moveToTargetPosition_E(currentPosition_E, Position_E, stepSize);
+      //int CurrentPosition_E = servo_E.read();  //讀取伺服馬達的當前位置
+      int CurrentPosition_E = LastPos[4];
+      moveToTargetPosition_E(CurrentPosition_E, Position_E, StepSize);
     
-      //int currentPosition_F = servo_F.read();  //讀取伺服馬達的當前位置
-      int currentPosition_F = LastPos[5];
-      moveToTargetPosition_F(currentPosition_F, Position_F, stepSize);
+      //int CurrentPosition_F = servo_F.read();  //讀取伺服馬達的當前位置
+      int CurrentPosition_F = LastPos[5];
+      moveToTargetPosition_F(CurrentPosition_F, Position_F, StepSize);
       
       PosSave [WriteAddress][0] = Position_A;
       PosSave [WriteAddress][1] = Position_B;
@@ -136,32 +137,32 @@ void loop() {
       Commed = 0;
       WriteAddress++;
       }
-    else if(Commed == 2 && WriteAddress > 0){
-        for (int i = 0; i < WriteAddress; i++){
+    else if (Commed == 2 && WriteAddress > 0) {
+        for (int i = 0; i < WriteAddress; i++) {
           ReadAddress = i;
-          //int currentPosition_A = servo_A.read();  //讀取伺服馬達的當前位置
-          int currentPosition_A = LastPos[0];
-          MovingTime_A = moveToTargetPosition_A(currentPosition_A, PosSave [ReadAddress][0], stepSize);
+          //int CurrentPosition_A = servo_A.read();  //讀取伺服馬達的當前位置
+          int CurrentPosition_A = LastPos[0];
+          MovingTime_A = moveToTargetPosition_A(CurrentPosition_A, PosSave [ReadAddress][0], StepSize);
           
-          //int currentPosition_B = servo_B.read();  //讀取伺服馬達的當前位置
-          int currentPosition_B = LastPos[1];
-          MovingTime_B = moveToTargetPosition_B(currentPosition_B, PosSave [ReadAddress][1], stepSize);
+          //int CurrentPosition_B = servo_B.read();  //讀取伺服馬達的當前位置
+          int CurrentPosition_B = LastPos[1];
+          MovingTime_B = moveToTargetPosition_B(CurrentPosition_B, PosSave [ReadAddress][1], StepSize);
         
-          //int currentPosition_C = servo_C.read();  //讀取伺服馬達的當前位置
-          int currentPosition_C = LastPos[2];
-          MovingTime_C = moveToTargetPosition_C(currentPosition_C, PosSave [ReadAddress][2], stepSize);
+          //int CurrentPosition_C = servo_C.read();  //讀取伺服馬達的當前位置
+          int CurrentPosition_C = LastPos[2];
+          MovingTime_C = moveToTargetPosition_C(CurrentPosition_C, PosSave [ReadAddress][2], StepSize);
         
-          //int currentPosition_D = servo_D.read();  //讀取伺服馬達的當前位置
-          int currentPosition_D = LastPos[3];
-          MovingTime_D = moveToTargetPosition_D(currentPosition_D, PosSave [ReadAddress][3], stepSize);
+          //int CurrentPosition_D = servo_D.read();  //讀取伺服馬達的當前位置
+          int CurrentPosition_D = LastPos[3];
+          MovingTime_D = moveToTargetPosition_D(CurrentPosition_D, PosSave [ReadAddress][3], StepSize);
         
-          //int currentPosition_E = servo_E.read();  //讀取伺服馬達的當前位置
-          int currentPosition_E = LastPos[4];
-          MovingTime_E = moveToTargetPosition_E(currentPosition_E, PosSave [ReadAddress][4], stepSize);
+          //int CurrentPosition_E = servo_E.read();  //讀取伺服馬達的當前位置
+          int CurrentPosition_E = LastPos[4];
+          MovingTime_E = moveToTargetPosition_E(CurrentPosition_E, PosSave [ReadAddress][4], StepSize);
         
-          //int currentPosition_F = servo_F.read();  //讀取伺服馬達的當前位置
-          int currentPosition_F = LastPos[5];
-          MovingTime_F = moveToTargetPosition_F(currentPosition_F, PosSave [ReadAddress][5], stepSize);
+          //int CurrentPosition_F = servo_F.read();  //讀取伺服馬達的當前位置
+          int CurrentPosition_F = LastPos[5];
+          MovingTime_F = moveToTargetPosition_F(CurrentPosition_F, PosSave [ReadAddress][5], StepSize);
     
           WaitTime = Moving_Max(MovingTime_A, MovingTime_B, MovingTime_C, MovingTime_D, MovingTime_E, MovingTime_F);
           
@@ -174,11 +175,11 @@ void loop() {
           Serial.print("F:" + String(PosSave [ReadAddress][0]) + " ");
           Serial.println("WaitTime:" + String(WaitTime));
           delay(WaitTime);
-        }
+          }
         ReadAddress = 0;
         Commed = 0;
-      }  
-    }
+    }  
+  }
 }
 
 void Servo_on() {
@@ -208,170 +209,170 @@ void Set_Servo(int Position_A, int Position_B, int Position_C, int Position_D, i
 
 int Moving_Max(int MovingTime_A, int MovingTime_B, int MovingTime_C, int MovingTime_D, int MovingTime_E, int MovingTime_F){
   int Time_Max = MovingTime_A;
-  if (MovingTime_B > Time_Max){
+  if (MovingTime_B > Time_Max) {
     Time_Max = MovingTime_B;
     }
-  if (MovingTime_C > Time_Max){
+  if (MovingTime_C > Time_Max) {
     Time_Max = MovingTime_C;
     }
-  if (MovingTime_D > Time_Max){
+  if (MovingTime_D > Time_Max) {
     Time_Max = MovingTime_D;
     }
-  if (MovingTime_E > Time_Max){
+  if (MovingTime_E > Time_Max) {
     Time_Max = MovingTime_E;
     }
-  if (MovingTime_F > Time_Max){
+  if (MovingTime_F > Time_Max) {
     Time_Max = MovingTime_F;
     }
   return Time_Max;
 }
 
-int moveToTargetPosition_A(int currentPosition, int targetPosition, int stepSize) {
-  //currentPosition = constrain(currentPosition, 0, 180);
-  int Moving = currentPosition - targetPosition;
+int moveToTargetPosition_A(int CurrentPosition, int TargetPosition, int StepSize) {
+  //CurrentPosition = constrain(CurrentPosition, 0, 180);
+  int Moving = CurrentPosition - TargetPosition;
   Moving = abs(Moving);
   Moving = Moving * Moving_delay;
-  Moving = Moving / stepSize;
+  Moving = Moving / StepSize;
   
-  
-  if (currentPosition < targetPosition) {
-    servo_A.write(currentPosition + stepSize); // 逼近目標位置
+  if (CurrentPosition < TargetPosition) {
+    servo_A.write(CurrentPosition + StepSize); // 逼近目標位置
     Serial.print("AF ");
     delay(Moving_delay); // 可選的延遲，以控制移動速度
-  } else if (currentPosition > targetPosition) {
-    servo_A.write(currentPosition - stepSize); // 逼近目標位置
+    }
+  else if (CurrentPosition > TargetPosition) {
+    servo_A.write(CurrentPosition - StepSize); // 逼近目標位置
     Serial.print("AR ");
     delay(Moving_delay); // 可選的延遲，以控制移動速度
-  }
-  LastPos[0] = targetPosition;
-  Serial.print(currentPosition);
+    }
+  LastPos[0] = TargetPosition;
+  Serial.print(CurrentPosition);
   Serial.print(" ");
-  Serial.print(targetPosition);
+  Serial.print(TargetPosition);
   Serial.print(" ");
   Serial.println(Moving);
   return Moving;
 }
 
-int moveToTargetPosition_B(int currentPosition, int targetPosition, int stepSize) {
-  int Moving = currentPosition - targetPosition;
+int moveToTargetPosition_B(int CurrentPosition, int TargetPosition, int StepSize) {
+  int Moving = CurrentPosition - TargetPosition;
   Moving = abs(Moving);
   Moving = Moving * Moving_delay;
-  Moving = Moving / stepSize;
+  Moving = Moving / StepSize;
   
-  
-  if (currentPosition < targetPosition) {
-    servo_B.write(currentPosition + stepSize); // 逼近目標位置
+  if (CurrentPosition < TargetPosition) {
+    servo_B.write(CurrentPosition + StepSize); // 逼近目標位置
     Serial.print("BF ");
     delay(Moving_delay); // 可選的延遲，以控制移動速度
-  } else if (currentPosition > targetPosition) {
-    servo_B.write(currentPosition - stepSize); // 逼近目標位置
+    }
+  else if (CurrentPosition > TargetPosition) {
+    servo_B.write(CurrentPosition - StepSize); // 逼近目標位置
     Serial.print("BR ");
     delay(Moving_delay); // 可選的延遲，以控制移動速度
-  }
-  LastPos[1] = targetPosition;
-  Serial.print(currentPosition);
+    }
+  LastPos[1] = TargetPosition;
+  Serial.print(CurrentPosition);
   Serial.print(" ");
-  Serial.print(targetPosition);
+  Serial.print(TargetPosition);
   Serial.print(" ");
   Serial.println(Moving);
   return Moving;
 }
 
-int moveToTargetPosition_C(int currentPosition, int targetPosition, int stepSize) {
-  int Moving = currentPosition - targetPosition;
+int moveToTargetPosition_C(int CurrentPosition, int TargetPosition, int StepSize) {
+  int Moving = CurrentPosition - TargetPosition;
   Moving = abs(Moving);
   Moving = Moving * Moving_delay;
-  Moving = Moving / stepSize;
+  Moving = Moving / StepSize;
   
-  
-  if (currentPosition < targetPosition) {
-    servo_C.write(currentPosition + stepSize); // 逼近目標位置
+  if (CurrentPosition < TargetPosition) {
+    servo_C.write(CurrentPosition + StepSize); // 逼近目標位置
     Serial.print("CF ");
     delay(Moving_delay); // 可選的延遲，以控制移動速度
-  } else if (currentPosition > targetPosition) {
-    servo_C.write(currentPosition - stepSize); // 逼近目標位置
+    }
+  else if (CurrentPosition > TargetPosition) {
+    servo_C.write(CurrentPosition - StepSize); // 逼近目標位置
     Serial.print("CR ");
     delay(Moving_delay); // 可選的延遲，以控制移動速度
-  }
-  LastPos[2] = targetPosition;LastPos[0] = targetPosition;
-  Serial.print(currentPosition);
+    }
+  LastPos[2] = TargetPosition;
+  Serial.print(CurrentPosition);
   Serial.print(" ");
-  Serial.print(targetPosition);
+  Serial.print(TargetPosition);
   Serial.print(" ");
   Serial.println(Moving);
   return Moving;
 }
 
-int moveToTargetPosition_D(int currentPosition, int targetPosition, int stepSize) {
-  int Moving = currentPosition - targetPosition;
+int moveToTargetPosition_D(int CurrentPosition, int TargetPosition, int StepSize) {
+  int Moving = CurrentPosition - TargetPosition;
   Moving = abs(Moving);
   Moving = Moving * Moving_delay;
-  Moving = Moving / stepSize;
+  Moving = Moving / StepSize;
   
-  
-  if (currentPosition < targetPosition) {
-    servo_D.write(currentPosition + stepSize); // 逼近目標位置
+  if (CurrentPosition < TargetPosition) {
+    servo_D.write(CurrentPosition + StepSize); // 逼近目標位置
     Serial.print("DF ");
     delay(Moving_delay); // 可選的延遲，以控制移動速度
-  } else if (currentPosition > targetPosition) {
-    servo_D.write(currentPosition - stepSize); // 逼近目標位置
+    }
+  else if (CurrentPosition > TargetPosition) {
+    servo_D.write(CurrentPosition - StepSize); // 逼近目標位置
     Serial.print("DR ");
     delay(Moving_delay); // 可選的延遲，以控制移動速度
-  }
-  LastPos[3] = targetPosition;
-  Serial.print(currentPosition);
+    }
+  LastPos[3] = TargetPosition;
+  Serial.print(CurrentPosition);
   Serial.print(" ");
-  Serial.print(targetPosition);
+  Serial.print(TargetPosition);
   Serial.print(" ");
   Serial.println(Moving);
   return Moving;
 }
 
-int moveToTargetPosition_E(int currentPosition, int targetPosition, int stepSize) {
-  int Moving = currentPosition - targetPosition;
+int moveToTargetPosition_E(int CurrentPosition, int TargetPosition, int StepSize) {
+  int Moving = CurrentPosition - TargetPosition;
   Moving = abs(Moving);
   Moving = Moving * Moving_delay;
-  Moving = Moving / stepSize;
+  Moving = Moving / StepSize;
   
-  
-  if (currentPosition < targetPosition) {
-    servo_E.write(currentPosition + stepSize); // 逼近目標位置
+  if (CurrentPosition < TargetPosition) {
+    servo_E.write(CurrentPosition + StepSize); // 逼近目標位置
     Serial.print("EF ");
     delay(Moving_delay); // 可選的延遲，以控制移動速度
-  } else if (currentPosition > targetPosition) {
-    servo_E.write(currentPosition - stepSize); // 逼近目標位置
+    }
+  else if (CurrentPosition > TargetPosition) {
+    servo_E.write(CurrentPosition - StepSize); // 逼近目標位置
     Serial.print("ER ");
     delay(Moving_delay); // 可選的延遲，以控制移動速度
-  }
-  LastPos[4] = targetPosition;
-  Serial.print(currentPosition);
+    }
+  LastPos[4] = TargetPosition;
+  Serial.print(CurrentPosition);
   Serial.print(" ");
-  Serial.print(targetPosition);
+  Serial.print(TargetPosition);
   Serial.print(" ");
   Serial.println(Moving);
   return Moving;
 }
 
-int moveToTargetPosition_F(int currentPosition, int targetPosition, int stepSize) {
-  int Moving = currentPosition - targetPosition;
+int moveToTargetPosition_F(int CurrentPosition, int TargetPosition, int StepSize) {
+  int Moving = CurrentPosition - TargetPosition;
   Moving = abs(Moving);
   Moving = Moving * Moving_delay;
-  Moving = Moving / stepSize;
+  Moving = Moving / StepSize;
   
-  
-  if (currentPosition < targetPosition) {
-    servo_F.write(currentPosition + stepSize); // 逼近目標位置
+  if (CurrentPosition < TargetPosition) {
+    servo_F.write(CurrentPosition + StepSize); // 逼近目標位置
     Serial.print("FF ");
     delay(Moving_delay); // 可選的延遲，以控制移動速度
-  } else if (currentPosition > targetPosition) {
-    servo_F.write(currentPosition - stepSize); // 逼近目標位置
+    }
+  else if (CurrentPosition > TargetPosition) {
+    servo_F.write(CurrentPosition - StepSize); // 逼近目標位置
     Serial.print("FR ");
     delay(Moving_delay); // 可選的延遲，以控制移動速度
-  }
-  LastPos[5] = targetPosition;
-  Serial.print(currentPosition);
+    }
+  LastPos[5] = TargetPosition;
+  Serial.print(CurrentPosition);
   Serial.print(" ");
-  Serial.print(targetPosition);
+  Serial.print(TargetPosition);
   Serial.print(" ");
   Serial.println(Moving);
   return Moving;
